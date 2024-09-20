8a78a3398c379164ec94af5233fe9bb054966107
Gabriel <147287432+gadosanjos@users.noreply.github.com>
2024-09-20 16:00:45 -0700

    Organized folders in preparation to ignore bin and obj folder

---
+++ 3-framework2/src/texturedTeapot.cpp
@@ -0,0 +1,176 @@
+#include <string.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+#include <iostream>
+
+#include <SOIL.h>
+#include <math.h>
+
+using namespace std;
+
+bool WireFrame= false;
+
+GLuint tex; // Texture pointer
+
+const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
+
+const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
+
+
+/* GLUT callback Handlers */
+
+void TextureLoader(const char* fileName);
+
+static void resize(int width, int height)
+{
+     double Ratio;
+
+   if(width<=height)
+            glViewport(0,(GLsizei) (height-width)/2,(GLsizei) width,(GLsizei) width);
+    else
+          glViewport((GLsizei) (width-height)/2 ,0 ,(GLsizei) height,(GLsizei) height);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+	gluPerspective (50.0f,1,0.1f, 100.0f);
+ }
+
+static void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    gluLookAt(0,5,10,0.0,0.0,0.0,0.0,1.0,0.0);
+
+    if(WireFrame)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);		//Draw Our Mesh In Wireframe Mesh
+	else
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);		//Toggle WIRE FRAME
+
+    // your code here
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glutSolidTeapot(1.5);
+
+    glutSwapBuffers();
+}
+
+static void key(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+        case 27 :
+        case 'q':
+            exit(0);
+            break;
+    }
+}
+
+void Specialkeys(int key, int x, int y)
+{
+    switch(key)
+    {
+    case GLUT_KEY_UP:
+    break;
+   }
+  glutPostRedisplay();
+}
+
+static void idle(void)
+{
+    glutPostRedisplay();
+}
+
+static void init(void)
+{
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL);
+
+    glEnable(GL_DEPTH_TEST);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    glShadeModel(GL_SMOOTH);
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+
+    //glEnable(GL_CULL_FACE);
+    //glCullFace(GL_BACK);
+
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_LIGHTING);
+
+    glGenTextures(1, &tex);
+    glEnable(GL_TEXTURE_2D);
+
+    TextureLoader("../images/t.jpg");
+    
+}
+
+/* Program entry point */
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+
+    glutInitWindowSize(800,600);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+
+    glutCreateWindow("GLUT Shapes");
+    init();
+    glutReshapeFunc(resize);
+    glutDisplayFunc(display);
+    glutKeyboardFunc(key);
+    glutSpecialFunc(Specialkeys);
+
+    glutIdleFunc(idle);
+    glutMainLoop();
+
+    return EXIT_SUCCESS;
+}
+
+void TextureLoader(const char* fileName){
+    unsigned char* image;              // Place your file name
+    int width, height;                 // width & height for the image reader
+
+    glBindTexture(GL_TEXTURE_2D, tex); // images are 2D arrays of pixels, bound to the GL_TEXTURE_2D target.
+
+    image = SOIL_load_image(fileName, &width, &height, 0, SOIL_LOAD_RGBA);
+
+    if(!image){
+        cout << "SOIL loading error: " << SOIL_last_result() << "' (" << fileName << ")" << endl;
+        return;
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    // biding image data
+    SOIL_free_image_data(image);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+}
+
+
